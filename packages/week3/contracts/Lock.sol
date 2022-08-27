c9d0382e21aa7f39d267fe0f8e56acb625ce5725
Lee <jessy1092@gmail.com>
2022-08-27 22:10:47 +0800

    chore: Setup prettier for solidity

--- packages/week3/contracts/Lock.sol
+++ packages/week3/contracts/Lock.sol
@@ -2,33 +2,30 @@
 pragma solidity ^0.8.16;
 
 // Import this file to use console.log
-import "hardhat/console.sol";
+import 'hardhat/console.sol';
 
 contract Lock {
-    uint256 public unlockTime;
-    address payable public owner;
+	uint256 public unlockTime;
+	address payable public owner;
 
-    event Withdrawal(uint256 amount, uint256 when);
+	event Withdrawal(uint256 amount, uint256 when);
 
-    constructor(uint256 _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+	constructor(uint256 _unlockTime) payable {
+		require(block.timestamp < _unlockTime, 'Unlock time should be in the future');
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
+		unlockTime = _unlockTime;
+		owner = payable(msg.sender);
+	}
 
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+	function withdraw() public {
+		// Uncomment this line to print a log in your terminal
+		// console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+		require(block.timestamp >= unlockTime, "You can't withdraw yet");
+		require(msg.sender == owner, "You aren't the owner");
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+		emit Withdrawal(address(this).balance, block.timestamp);
 
-        owner.transfer(address(this).balance);
-    }
+		owner.transfer(address(this).balance);
+	}
 }
