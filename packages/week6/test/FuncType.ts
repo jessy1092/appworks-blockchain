177c32e5fcfe21d52858211bdd7115079c460c0b
Lee <jessy1092@gmail.com>
2022-09-18 00:08:48 +0800

    feat: Implement FuncType for week6

---
+++ packages/week6/test/FuncType.ts
@@ -0,0 +1,32 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('FuncType', function () {
+	// We define a fixture to reuse the same setup in every test.
+	// We use loadFixture to run this setup once, snapshot that state,
+	// and reset Hardhat Network to that snapshot in every test.
+	async function deployOneYearLockFixture() {
+		// Contracts are deployed using the first signer/account by default
+		const [owner, otherAccount] = await ethers.getSigners();
+
+		const Basic = await ethers.getContractFactory('FuncType');
+		const basic = await Basic.deploy();
+
+		return { basic, owner, otherAccount };
+	}
+
+	describe('Deployment', function () {
+		it('Should set the right owner', async function () {
+			const { basic, owner } = await loadFixture(deployOneYearLockFixture);
+
+			expect(await basic.owner()).to.equal(owner.address);
+		});
+
+		it('Should receive and store the funds to lock', async function () {
+			const { basic } = await loadFixture(deployOneYearLockFixture);
+
+			expect(await ethers.provider.getBalance(basic.address)).to.equal(0);
+		});
+	});
+});
