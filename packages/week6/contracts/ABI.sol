468c4a58bc1eb50e363965b779c77ab065c78947
Lee <jessy1092@gmail.com>
2022-09-18 00:27:01 +0800

    feat: Implement encode/decode for week6

---
+++ packages/week6/contracts/ABI.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract FuncType {
+	address payable public owner;
+
+	struct Person {
+		string name;
+		uint8 age;
+	}
+
+	constructor() {
+		owner = payable(msg.sender);
+	}
+
+	function encode(
+		uint256 x,
+		address addr,
+		uint256[] calldata arr,
+		Person calldata person
+	) external pure returns (bytes memory) {
+		return abi.encode(x, addr, arr, person);
+	}
+
+	function decode(bytes memory data)
+		external
+		pure
+		returns (
+			uint256,
+			address,
+			uint256[] memory,
+			Person memory
+		)
+	{
+		(uint256 x, address addr, uint256[] memory arr, Person memory person) = abi.decode(
+			data,
+			(uint256, address, uint256[], Person)
+		);
+
+		return (x, addr, arr, person);
+	}
+}
