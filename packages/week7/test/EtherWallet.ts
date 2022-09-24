7b27d2604d964f41d8507b73e088d27d093e3941
Lee <jessy1092@gmail.com>
2022-09-24 23:55:58 +0800

    feat: Implement the EtherWallet contract

---
+++ packages/week7/test/EtherWallet.ts
@@ -0,0 +1,53 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('MultiSigWallet', function () {
+	const DefaultValue = 1_000_000_000;
+	// We define a fixture to reuse the same setup in every test.
+	// We use loadFixture to run this setup once, snapshot that state,
+	// and reset Hardhat Network to that snapshot in every test.
+	async function deployOneYearLockFixture() {
+		// Contracts are deployed using the first signer/account by default
+		const [owner] = await ethers.getSigners();
+
+		const Basic = await ethers.getContractFactory('EtherWallet');
+		const basic = await Basic.deploy({ value: DefaultValue });
+
+		return { basic, owner, DefaultValue };
+	}
+
+	describe('Deployment', function () {
+		it('Should set the right owner', async function () {
+			const { basic, owner } = await loadFixture(deployOneYearLockFixture);
+
+			expect(await basic.owner()).to.equal(owner.address);
+		});
+
+		it('Should receive and store the funds to lock', async function () {
+			const { basic, DefaultValue } = await loadFixture(deployOneYearLockFixture);
+
+			expect(await ethers.provider.getBalance(basic.address)).to.equal(DefaultValue);
+		});
+	});
+
+	describe('Deposit/Withdraw', function () {
+		it('Could deposit ether', async function () {
+			const DepositValue = 1_000_000_000;
+
+			const { basic, DefaultValue } = await loadFixture(deployOneYearLockFixture);
+
+			await basic.deposit({ value: DepositValue });
+
+			expect(await ethers.provider.getBalance(basic.address)).to.equal(DepositValue + DefaultValue);
+		});
+
+		it('Could withdraw ether', async function () {
+			const { basic, DefaultValue } = await loadFixture(deployOneYearLockFixture);
+
+			await basic.withdraw(DefaultValue);
+
+			expect(await ethers.provider.getBalance(basic.address)).to.equal(0);
+		});
+	});
+});
