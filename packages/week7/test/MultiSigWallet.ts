b0019f7979a6032c9a69de78d52c0b6a7154a1dc
Lee <jessy1092@gmail.com>
2022-09-24 17:29:13 +0800

    feat: Implement the MultiSigWallet

---
+++ packages/week7/test/MultiSigWallet.ts
@@ -0,0 +1,68 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('MultiSigWallet', function () {
+	// We define a fixture to reuse the same setup in every test.
+	// We use loadFixture to run this setup once, snapshot that state,
+	// and reset Hardhat Network to that snapshot in every test.
+	async function deployOneYearLockFixture() {
+		// Contracts are deployed using the first signer/account by default
+		const [owner, ...otherAccount] = await ethers.getSigners();
+
+		const Basic = await ethers.getContractFactory('MultiSigWallet');
+		const basic = await Basic.deploy(
+			[owner.address, otherAccount[0].address, otherAccount[1].address],
+			2,
+		);
+
+		return { basic, owner, otherAccount };
+	}
+
+	describe('Deployment', function () {
+		it('Should set the right owners and required', async function () {
+			const { basic, owner, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+			const owner1 = await basic.owners(0);
+			const owner2 = await basic.owners(1);
+			const owner3 = await basic.owners(2);
+			const required = await basic.required();
+
+			expect(owner1).to.equal(owner.address);
+			expect(owner2).to.equal(otherAccount[0].address);
+			expect(owner3).to.equal(otherAccount[1].address);
+			expect(required).to.equal(2);
+		});
+
+		it('Should receive and store the funds to lock', async function () {
+			const { basic } = await loadFixture(deployOneYearLockFixture);
+
+			expect(await ethers.provider.getBalance(basic.address)).to.equal(0);
+		});
+	});
+
+	describe('Approve', function () {
+		it('Should approve with owner', async function () {
+			const { basic, owner, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+			await basic.submit(otherAccount[3].address, 1, '0x00');
+			await basic.approve(0);
+
+			const approve = await basic.approved(0, owner.address);
+
+			expect(approve).to.equal(true);
+		});
+
+		it('Should approved by other owner', async function () {
+			const { basic, owner, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+			await basic.submit(otherAccount[3].address, 1, '0x00');
+
+			await basic.connect(otherAccount[0]).approve(0);
+
+			const approve = await basic.approved(0, otherAccount[0].address);
+
+			expect(approve).to.equal(true);
+		});
+	});
+});
