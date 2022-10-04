221179bdf208445bb5d378a6874e78e29c14d1e5
Lee <jessy1092@gmail.com>
2022-10-04 18:31:27 +0800

    feat: Implement the earlyMint with whitelist that create by MerkleTree

---
+++ packages/week9/contracts/AppWorksV2.sol
@@ -0,0 +1,197 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.16;
+
+import '@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/utils/cryptography/MerkleProofUpgradeable.sol';
+
+contract AppWorksV2 is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+	using StringsUpgradeable for uint256;
+
+	using CountersUpgradeable for CountersUpgradeable.Counter;
+	CountersUpgradeable.Counter private _nextTokenId;
+
+	uint256 public price;
+	uint256 public maxSupply;
+
+	bool public mintActive;
+	bool public earlyMintActive;
+	bool public revealed;
+
+	string public baseURI;
+	bytes32 public merkleRoot;
+
+	mapping(uint256 => string) private _tokenURIs;
+	mapping(address => uint256) public addressMintedBalance;
+
+	// Set mint per user limit to 10 and owner limit to 20 - Week 8
+	uint256 public mintNumberPerUser;
+	uint256 public mintNumberPerOwner;
+
+	// constructor() ERC721('AppWorks', 'AW') Ownable() {}
+
+	modifier checkMintNumber(uint256 _mintAmount) {
+		//Check how many NFTs are available to be minted
+		uint256 mintCount = addressMintedBalance[msg.sender];
+		uint256 mintNumber = owner() == msg.sender ? mintNumberPerOwner : mintNumberPerUser;
+		require(mintCount + _mintAmount <= mintNumber, 'Excess maximun number of mint');
+
+		// Check non-zero mint
+		require(_mintAmount > 0, 'Zero mint');
+
+		// Check maxSupply
+		uint256 currentIndex = _nextTokenId.current();
+		require(_mintAmount + currentIndex <= maxSupply, 'Excess maximun supply');
+
+		_;
+	}
+
+	modifier checkMintFund(uint256 _mintAmount) {
+		//Check user has sufficient funds
+		require(_mintAmount * price == msg.value, 'Not match ETH with price');
+
+		_;
+	}
+
+	function initialize() public initializer {
+		__ERC721_init('AppWorks', 'AW');
+		__Ownable_init();
+		__UUPSUpgradeable_init();
+
+		__init();
+	}
+
+	function __init() internal initializer {
+		price = 0.01 ether;
+		mintActive = false;
+		earlyMintActive = false;
+		revealed = false;
+		maxSupply = 100;
+		mintNumberPerUser = 10;
+		mintNumberPerOwner = 20;
+	}
+
+	function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+	function _batchMint(uint256 _mintAmount) internal {
+		addressMintedBalance[msg.sender] += _mintAmount;
+		uint256 currentIndex = _nextTokenId.current();
+
+		for (uint256 i = 1; i <= _mintAmount; i++) {
+			_nextTokenId.increment();
+			_safeMint(msg.sender, currentIndex + i);
+		}
+	}
+
+	// Public mint function - week 8
+	function mint(uint256 _mintAmount)
+		public
+		payable
+		checkMintNumber(_mintAmount)
+		checkMintFund(_mintAmount)
+	{
+		//Please make sure you check the following things:
+		//Current state is available for Public Mint
+		require(mintActive, 'Inactive Mint');
+
+		// Start mint
+		_batchMint(_mintAmount);
+	}
+
+	// Implement totalSupply() Function to return current total NFT being minted - week 8
+	function totalSupply() external view returns (uint256) {
+		return _nextTokenId.current();
+	}
+
+	// Implement withdrawBalance() Function to withdraw funds from the contract - week 8
+	function withdrawBalance() external onlyOwner {
+		(bool success, ) = payable(owner()).call{ value: address(this).balance }('');
+
+		require(success, 'Failed to withdraw balance');
+	}
+
+	// Implement setPrice(price) Function to set the mint price - week 8
+	function setPrice(uint256 _price) external onlyOwner {
+		price = _price;
+	}
+
+	// Implement toggleMint() Function to toggle the public mint available or not - week 8
+	function toggleMint() external onlyOwner {
+		mintActive = !mintActive;
+	}
+
+	// Implement toggleReveal() Function to toggle the blind box is revealed - week 9
+	function toggleReveal() external onlyOwner {
+		revealed = !revealed;
+	}
+
+	function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+		_requireMinted(tokenId);
+
+		string memory mBaseURI = _baseURI();
+
+		if (!revealed) {
+			return bytes(mBaseURI).length > 0 ? string(abi.encodePacked(mBaseURI, 'blind')) : '';
+		}
+		return
+			bytes(mBaseURI).length > 0
+				? string(abi.encodePacked(mBaseURI, tokenId.toString()))
+				: '';
+	}
+
+	// Implement setBaseURI(newBaseURI) Function to set BaseURI - week 9
+	function setBaseURI(string calldata _newBaseURI) external onlyOwner {
+		baseURI = _newBaseURI;
+	}
+
+	// Function to return the base URI
+	function _baseURI() internal view virtual override returns (string memory) {
+		return baseURI;
+	}
+
+	// Early mint function for people on the whitelist - week 9
+	function earlyMint(bytes32[] calldata _merkleProof, uint256 _mintAmount)
+		public
+		payable
+		checkMintNumber(_mintAmount)
+		checkMintFund(_mintAmount)
+	{
+		//Please make sure you check the following things:
+		//Current state is available for Early Mint
+		require(earlyMintActive, 'Inactive Early Mint');
+		//Check how many NFTs are available to be minted
+		// Use checkMintNumber(_mintAmount)
+
+		//Check user is in the whitelist - use merkle tree to validate
+		bool proofSuccess = checkInWhitelist(_merkleProof);
+
+		require(proofSuccess, 'Not in whitelist');
+		//Check user has sufficient funds
+		// Use checkMintFund(_mintAmount)
+
+		// Start mint
+		_batchMint(_mintAmount);
+	}
+
+	function checkInWhitelist(bytes32[] calldata _merkleProof) public view returns (bool) {
+		bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+		return MerkleProofUpgradeable.verifyCalldata(_merkleProof, merkleRoot, leaf);
+	}
+
+	// Implement toggleEarlyMint() Function to toggle the early mint available or not - week 9
+	function toggleEarlyMint() external onlyOwner {
+		earlyMintActive = !earlyMintActive;
+	}
+
+	// Implement setMerkleRoot(merkleRoot) Function to set new merkle root - week 9
+	function setMerkleRoot(bytes32 _root) external onlyOwner {
+		merkleRoot = _root;
+	}
+
+	// Let this contract can be upgradable, using openzepplin proxy library - week 10
+	// Try to modify blind box images by using proxy
+}
--- packages/week9/test/AppWorks.proxy.ts
+++ packages/week9/test/AppWorks.proxy.ts
@@ -1,38 +1,57 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers, upgrades } from 'hardhat';
-import BigNumber from 'bignumber.js';
+import keccak256 from 'keccak256';
 
-import { AppWorks } from '../test-types/contracts/AppWorks';
+import { createMerkleTree } from '../node-whitelist';
+
+import { AppWorksV2 } from '../test-types/contracts/AppWorksV2';
 
 describe('AppWorks', function () {
 	// We define a fixture to reuse the same setup in every test.
 	// We use loadFixture to run this setup once, snapshot that state,
 	// and reset Hardhat Network to that snapshot in every test.
-	async function deployOneYearLockFixture() {
+	async function deployAppWorksFixture() {
 		const DEFAULT_MINT_PRICE = ethers.utils.parseEther('0.01');
 		// Contracts are deployed using the first signer/account by default
-		const [owner, ...otherAccount] = await ethers.getSigners();
+		const allAccounts = await ethers.getSigners();
 
-		const Basic = await ethers.getContractFactory('AppWorks');
+		const [owner, ...otherAccount] = allAccounts;
+
+		const Basic = await ethers.getContractFactory('AppWorksV2');
 		const basic = (await upgrades.deployProxy(Basic, {
 			initializer: 'initialize',
 			kind: 'uups',
-		})) as AppWorks;
+		})) as AppWorksV2;
 
-		return { DEFAULT_MINT_PRICE, basic, owner, otherAccount };
+		return { DEFAULT_MINT_PRICE, basic, owner, otherAccount, allAccounts };
+	}
+
+	async function createWhitelistFixture() {
+		const { allAccounts, ...other } = await deployAppWorksFixture();
+
+		const addresses = allAccounts.slice(0, 19).map(account => account.address);
+
+		const merkleTree = createMerkleTree(addresses);
+
+		return {
+			merkleTree,
+			merkleRoot: `0x${merkleTree.getRoot().toString('hex')}`,
+			notInWhitelistAccount: allAccounts[19],
+			...other,
+		};
 	}
 
 	describe('Deployment', function () {
 		it('Should set the right name and symbol ', async function () {
-			const { basic } = await loadFixture(deployOneYearLockFixture);
+			const { basic } = await loadFixture(deployAppWorksFixture);
 
 			expect(await basic.name()).to.equal('AppWorks');
 			expect(await basic.symbol()).to.equal('AW');
 		});
 
 		it('Should balance zero', async function () {
-			const { basic } = await loadFixture(deployOneYearLockFixture);
+			const { basic } = await loadFixture(deployAppWorksFixture);
 
 			expect(await ethers.provider.getBalance(basic.address)).to.equal(0);
 		});
@@ -40,7 +59,7 @@ describe('AppWorks', function () {
 
 	describe('Mint', function () {
 		it('Should revert with mintActive = false', async function () {
-			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await expect(basic.mint(1, { value: DEFAULT_MINT_PRICE })).to.be.revertedWith(
 				'Inactive Mint',
@@ -48,7 +67,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should revert with excess maximun number of mint', async function () {
-			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -58,9 +77,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should be 10 of the maximun number of mint by user', async function () {
-			const { basic, otherAccount, DEFAULT_MINT_PRICE } = await loadFixture(
-				deployOneYearLockFixture,
-			);
+			const { basic, otherAccount, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -70,7 +87,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should match addressMintedBalance after mint', async function () {
-			const { basic, owner, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, owner, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -80,7 +97,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should mint many time if not excess maximun number of mint', async function () {
-			const { basic, owner, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, owner, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -91,9 +108,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should revert with excess maximun supply', async function () {
-			const { basic, otherAccount, DEFAULT_MINT_PRICE } = await loadFixture(
-				deployOneYearLockFixture,
-			);
+			const { basic, otherAccount, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -113,7 +128,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should revert with zero mint', async function () {
-			const { basic } = await loadFixture(deployOneYearLockFixture);
+			const { basic } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -121,7 +136,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should revert with price not match', async function () {
-			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -131,7 +146,7 @@ describe('AppWorks', function () {
 		});
 
 		it('Should match totalSupply and value after mint', async function () {
-			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -144,7 +159,7 @@ describe('AppWorks', function () {
 
 	describe('setPrice', function () {
 		it('Should change mint price', async function () {
-			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -162,7 +177,7 @@ describe('AppWorks', function () {
 
 	describe('withdrawBalance', function () {
 		it('Should withdraw balance', async function () {
-			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployOneYearLockFixture);
+			const { basic, DEFAULT_MINT_PRICE } = await loadFixture(deployAppWorksFixture);
 
 			await basic.toggleMint();
 
@@ -173,6 +188,246 @@ describe('AppWorks', function () {
 			await basic.withdrawBalance();
 
 			expect(await ethers.provider.getBalance(basic.address)).to.equal(0);
+		});
+	});
+
+	describe('checkInWhitelist', function () {
+		it('Should set merkleRoot with setMerkleRoot', async function () {
+			const { basic, merkleRoot } = await loadFixture(createWhitelistFixture);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			expect(await basic.merkleRoot()).to.equal(merkleRoot);
+		});
+
+		it('Should return true when pass correct proof', async function () {
+			const { basic, owner, merkleTree, merkleRoot } = await loadFixture(createWhitelistFixture);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.checkInWhitelist(proof);
+
+			expect(await basic.checkInWhitelist(proof)).to.equal(true);
+		});
+
+		it('Should return false when not pass correct proof', async function () {
+			const { basic, notInWhitelistAccount, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(notInWhitelistAccount.address));
+
+			await basic.connect(notInWhitelistAccount).checkInWhitelist(proof);
+
+			expect(await basic.checkInWhitelist(proof)).to.equal(false);
+		});
+	});
+
+	describe('earlyMint', function () {
+		it('Should revert with mintActive = false', async function () {
+			const { basic, owner, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await expect(basic.earlyMint(proof, 1, { value: DEFAULT_MINT_PRICE })).to.be.revertedWith(
+				'Inactive Early Mint',
+			);
+		});
+
+		it('Should revert with excess maximun number of mint', async function () {
+			const { basic, owner, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await expect(
+				basic.earlyMint(proof, 21, { value: DEFAULT_MINT_PRICE.mul(21) }),
+			).to.be.revertedWith('Excess maximun number of mint');
+		});
+
+		it('Should revert with not in whitelist', async function () {
+			const { basic, notInWhitelistAccount, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } =
+				await loadFixture(createWhitelistFixture);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(notInWhitelistAccount.address));
+
+			await basic.toggleEarlyMint();
+
+			await expect(
+				basic
+					.connect(notInWhitelistAccount)
+					.earlyMint(proof, 1, { value: DEFAULT_MINT_PRICE.mul(1) }),
+			).to.be.revertedWith('Not in whitelist');
+		});
+
+		it('Should be 10 of the maximun number of mint by user', async function () {
+			const { basic, otherAccount, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+			await basic.setMerkleRoot(merkleRoot);
+
+			const normalAccount = otherAccount[0];
+			const proof = merkleTree.getHexProof(keccak256(normalAccount.address));
+
+			await basic.toggleEarlyMint();
+
+			await expect(
+				basic.connect(normalAccount).earlyMint(proof, 11, { value: DEFAULT_MINT_PRICE.mul(11) }),
+			).to.be.revertedWith('Excess maximun number of mint');
+		});
+
+		it('Should match addressMintedBalance after mint', async function () {
+			const { basic, owner, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await basic.earlyMint(proof, 2, { value: DEFAULT_MINT_PRICE.mul(2) });
+
+			expect(await basic.addressMintedBalance(owner.address)).to.equal('2');
+		});
+
+		it('Should mint many time if not excess maximun number of mint', async function () {
+			const { basic, owner, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await basic.earlyMint(proof, 1, { value: DEFAULT_MINT_PRICE });
+			await basic.earlyMint(proof, 2, { value: DEFAULT_MINT_PRICE.mul(2) });
+
+			expect(await basic.addressMintedBalance(owner.address)).to.equal('3');
+		});
+
+		it('Should revert with excess maximun supply', async function () {
+			const { basic, owner, otherAccount, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } =
+				await loadFixture(createWhitelistFixture);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await basic.earlyMint(proof, 20, { value: DEFAULT_MINT_PRICE.mul(20) });
+			await basic
+				.connect(otherAccount[0])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[0].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[1])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[1].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[2])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[2].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[3])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[3].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[4])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[4].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[5])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[5].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[6])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[6].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+			await basic
+				.connect(otherAccount[7])
+				.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[7].address)), 10, {
+					value: DEFAULT_MINT_PRICE.mul(10),
+				});
+
+			await expect(
+				basic
+					.connect(otherAccount[10])
+					.earlyMint(merkleTree.getHexProof(keccak256(otherAccount[10].address)), 1, {
+						value: DEFAULT_MINT_PRICE,
+					}),
+			).to.be.revertedWith('Excess maximun supply');
+		});
+
+		it('Should revert with zero mint', async function () {
+			const { basic, owner, merkleTree, merkleRoot } = await loadFixture(createWhitelistFixture);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await expect(basic.earlyMint(proof, 0)).to.be.revertedWith('Zero mint');
+		});
+
+		it('Should revert with price not match', async function () {
+			const { basic, owner, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await expect(basic.earlyMint(proof, 2, { value: DEFAULT_MINT_PRICE })).to.be.revertedWith(
+				'Not match ETH with price',
+			);
+		});
+
+		it('Should match totalSupply and value after mint', async function () {
+			const { basic, owner, DEFAULT_MINT_PRICE, merkleTree, merkleRoot } = await loadFixture(
+				createWhitelistFixture,
+			);
+
+			await basic.setMerkleRoot(merkleRoot);
+
+			const proof = merkleTree.getHexProof(keccak256(owner.address));
+
+			await basic.toggleEarlyMint();
+
+			await basic.earlyMint(proof, 2, { value: DEFAULT_MINT_PRICE.mul(2) });
+
+			expect(await basic.totalSupply()).to.equal('2');
+			expect(await ethers.provider.getBalance(basic.address)).to.equal(DEFAULT_MINT_PRICE.mul(2));
 		});
 	});
 });
