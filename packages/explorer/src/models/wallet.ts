57aae3fd7288710a138ce896f57c7f5c8dcc50c5
Lee <jessy1092@gmail.com>
2022-10-06 14:07:28 +0800

    refactor: Extra useConnectWeb3 hook

--- packages/explorer/src/models/wallet.ts
+++ packages/explorer/src/models/wallet.ts
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Web3 from 'web3';
+import { AbstractProvider } from 'web3-core';
 
 export const isSupportWeb3 =
 	typeof window.ethereum !== 'undefined' && typeof window.ethereum.request !== 'undefined';
@@ -29,4 +30,44 @@ export const useWallet = () => {
 		isActive: wallet !== null,
 		wallet,
 	};
+};
+
+export const useConnectWeb3 = () => {
+	const [myWallet, setWallet] = useState<null | Web3>(null);
+	const [address, setAddress] = useState('');
+
+	const connect = async () => {
+		if (isSupportWeb3) {
+			await window.ethereum?.request?.({ method: 'eth_requestAccounts' });
+			const web3 = new Web3(window.ethereum as AbstractProvider);
+
+			setWallet(web3);
+
+			const account = web3.eth.accounts;
+			//Get the current MetaMask selected/active wallet
+			const walletAddress = account.givenProvider.selectedAddress;
+			console.log(`Wallet: ${walletAddress}`);
+			setAddress(walletAddress);
+		} else {
+			console.log('No wallet');
+		}
+	};
+
+	useEffect(() => {
+		const fetchAccount = async () => {
+			const address = await checkConnection();
+			console.log(address);
+			setAddress(address);
+
+			if (address !== '') {
+				const web3 = new Web3(window.ethereum as AbstractProvider);
+
+				setWallet(web3);
+			}
+		};
+
+		fetchAccount();
+	}, []);
+
+	return { myWallet, address, connect };
 };
--- packages/explorer/src/routes/Test-ERC4626/component.tsx
+++ packages/explorer/src/routes/Test-ERC4626/component.tsx
@@ -1,48 +1,10 @@
-import { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import { AbstractProvider } from 'web3-core';
-
 import styles from './index.module.css';
 
 import Dashboard from 'components/Dashboard';
-import { checkConnection, isSupportWeb3, Wallet } from 'models/wallet';
+import { isSupportWeb3, useConnectWeb3, Wallet } from 'models/wallet';
 
 function App() {
-	const [myWallet, setWallet] = useState<null | Web3>(null);
-	const [address, setAddress] = useState('');
-
-	const connect = async () => {
-		if (isSupportWeb3) {
-			await window.ethereum?.request?.({ method: 'eth_requestAccounts' });
-			const web3 = new Web3(window.ethereum as AbstractProvider);
-
-			setWallet(web3);
-
-			const account = web3.eth.accounts;
-			//Get the current MetaMask selected/active wallet
-			const walletAddress = account.givenProvider.selectedAddress;
-			console.log(`Wallet: ${walletAddress}`);
-			setAddress(walletAddress);
-		} else {
-			console.log('No wallet');
-		}
-	};
-
-	useEffect(() => {
-		const fetchAccount = async () => {
-			const address = await checkConnection();
-			console.log(address);
-			setAddress(address);
-
-			if (address !== '') {
-				const web3 = new Web3(window.ethereum as AbstractProvider);
-
-				setWallet(web3);
-			}
-		};
-
-		fetchAccount();
-	}, []);
+	const { myWallet, address, connect } = useConnectWeb3();
 
 	return (
 		<Wallet.Provider value={myWallet}>
