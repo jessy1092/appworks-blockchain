55bc8f00b96d7132bef394d647a763188c2868e4
Lee <jessy1092@gmail.com>
2022-10-06 17:27:53 +0800

    feat: Implement the generate whitelist script

--- packages/week9/node-whitelist.ts
+++ packages/week9/node-whitelist.ts
@@ -1,5 +1,8 @@
 import { MerkleTree } from 'merkletreejs';
 import keccak256 from 'keccak256';
+import { writeFile } from 'node:fs/promises';
+
+import whitelist from './whitelist.json';
 
 export const createMerkleTree = (addresses: string[]): MerkleTree => {
 	let leaves = addresses.map(addr => keccak256(addr));
@@ -11,3 +14,23 @@ export const createMerkleTree = (addresses: string[]): MerkleTree => {
 
 	return merkleTree;
 };
+
+interface MerkleData {
+	[id: string]: string | string[];
+}
+
+const run = async () => {
+	const merkleTree = createMerkleTree(whitelist);
+
+	const data: MerkleData = {
+		merkleRoot: `0x${merkleTree.getRoot().toString('hex')}`,
+	};
+
+	whitelist.forEach(address => {
+		data[address] = merkleTree.getHexProof(keccak256(address));
+	});
+
+	await writeFile('whitelist-merkleTree.json', JSON.stringify(data));
+};
+
+run();
