a08bcb8ceb7ab2f1fba9e134859236551f487bf6
Lee <jessy1092@gmail.com>
2022-10-12 15:54:55 +0800

    feat: Filter with my NFT list

--- packages/explorer/src/routes/Appworks/Gallery/component.tsx
+++ packages/explorer/src/routes/Appworks/Gallery/component.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { useAppWorks } from 'models/appworks';
+import { useAppWorks, useMyAppWorks } from 'models/appworks';
 import { useConnectWeb3, Wallet } from 'models/wallet';
 
 import styles from './index.module.css';
@@ -8,26 +8,50 @@ import ButtonConnect from 'components/ButtonConnect';
 import { range } from 'utility';
 import Gallery from 'components/Gallery';
 
-const Home: React.FC = () => {
-	const appWorksState = useAppWorks();
-	const { myWallet, address, connect } = useConnectWeb3();
+interface ContainerProp {
+	address: string;
+}
 
-	const allData = range(1, appWorksState.totalSupply);
+const Container: React.FC<ContainerProp> = ({ address }) => {
+	const appWorksState = useAppWorks();
+	const [filterMyNFT, setFilterMyNFT] = useState<boolean>(false);
+	const { myAppWorksState } = useMyAppWorks(address);
+
+	const data = filterMyNFT
+		? myAppWorksState.addressMintedBalance
+		: range(1, appWorksState.totalSupply);
+
+	const onToggle = () => {
+		setFilterMyNFT(!filterMyNFT);
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.head}>
+				<h2>{filterMyNFT ? 'My' : 'All'} NFT </h2>
+				{address !== '' && (
+					<button onClick={onToggle}>{filterMyNFT ? 'Show All NFT' : 'Show My NFT'}</button>
+				)}
+			</div>
+			<div className={styles.list}>
+				<Gallery
+					data={data}
+					revealed={appWorksState.revealed}
+					baseURI={appWorksState.baseURI}
+				></Gallery>
+			</div>
+		</div>
+	);
+};
+
+const Home: React.FC = () => {
+	const { myWallet, address, connect } = useConnectWeb3();
 
 	return (
 		<Wallet.Provider value={myWallet}>
 			<div className={styles.gallery}>
 				<ButtonConnect address={address} connect={connect}></ButtonConnect>
-				<div className={styles.container}>
-					<h2>All NFT</h2>
-					<div className={styles.list}>
-						<Gallery
-							data={allData}
-							revealed={appWorksState.revealed}
-							baseURI={appWorksState.baseURI}
-						></Gallery>
-					</div>
-				</div>
+				<Container address={address}></Container>
 			</div>
 		</Wallet.Provider>
 	);
